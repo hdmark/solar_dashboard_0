b86e1ae83239d818b2623821f75f7e06594e328b
mark baskin <mark.d.baskin@gmail.com>
2025-12-09 13:54:29 -0500

    node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,18 +8,12 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html", // SPA fallback for nginx
-      precompress: false,
-      strict: false, // allow dynamic routes with fallback
-    }),
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
     alias: {
-      "@/*": "src/lib/*",
-    },
-    prerender: {
-      entries: [],
+      "@/*": "./path/to/lib/*",
     },
   },
 };
